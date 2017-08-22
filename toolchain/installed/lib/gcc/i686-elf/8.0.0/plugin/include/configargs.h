2038c79df5dbd09f7a45134dd6f33c14bd36a3a2
jwhite <jwhite@laptop.lan>
2017-08-21 19:06:05 -0700

    Remove compiled toolchain

--- toolchain/installed/lib/gcc/i686-elf/8.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-8-20170813/configure --prefix=/home/jwhite/os/toolchain/installed --enable-ld=yes --enable-languages=c,c++ --disable-libstdcxx --disable-bootstrap --disable-libssp --disable-libquadmath --disable-libada --target=i686-elf";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
