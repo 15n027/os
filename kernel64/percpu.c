1b5c6346dd86c755d872a3612b1d000241132f04
jwhite <jwhite@laptop.lan>
2019-07-27 09:19:16 -0700

    Set GS base searlier to enable scratch

--- kernel64/percpu.c
+++ kernel64/percpu.c
@@ -18,12 +18,13 @@ PerCpuInit(bool isBSP)
     } else {
         NOT_IMPLEMENTED();
     }
+    WRMSR(IA32_GS_BASE, (uintptr_t)&pPerCpu);
     // TODO - Use cpu number
     perCpu->scratchStart = SCRATCH_START + offset;
     perCpu->scratchUsed = 0;
+    DBG("scratchstart=%lx", perCpu->scratchStart);
     for (unsigned i = 0; i < 512; i++) {
         map_page(0, perCpu->scratchStart + PAGE_SIZE * i, 0);
     }
-    asm("" ::: "memory");
-    WRMSR(IA32_GS_BASE, (uintptr_t)&pPerCpu);
+    DBG("");
 }
